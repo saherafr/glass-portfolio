659128f7455ac1b65e08cbc813d6e2fdac304875
saherafr <118156614+saherafr@users.noreply.github.com>
2025-05-15 17:56:46 -0600

    Update Portfolio.jsx

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./portfolio.css";
 import IMG1 from "../../assets/portfolio1.jpg";
 import IMG2 from "../../assets/portfolio2.jpg";
-import IMG3 from "../../assets/portfolio3.jpg";
+import IMG3 from "../../assets/portfolio3.JPG";
 
 const data = [
     {
