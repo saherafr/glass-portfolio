082191774a30abbc3e5fbc7481ec521f75896b79
saherafr <118156614+saherafr@users.noreply.github.com>
2025-05-26 23:17:33 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,24 @@ import About from "./components/about/About";
 import Experience from "./components/experience/Experience";
 import Header from "./components/header/Header";
 import Nav from "./components/nav/Nav";
-//import Services from "./components/services/Services";
 import Portfolio from "./components/portfolio/Portfolio";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
-//import Testimonials from "./components/testmonials/Testimonials";
+import Leetcode from "./components/leetcode/Leetcode";
 
 const App = () => {
     return (
         <>
-         <Header />
-         <Nav/>
-         <About/>
-         <Experience/>
-         {/* <Services/> */}
-         <Portfolio/>
-         {/* <Testimonials/> */}
-         <Contact/>
-         <Footer/>
+            <Header />
+            <Nav />
+            <About />
+            <Portfolio />
+            <Leetcode />
+            <Experience />
+            <Contact />
+            <Footer />
         </>
     );
-    }
+};
 
 export default App;
